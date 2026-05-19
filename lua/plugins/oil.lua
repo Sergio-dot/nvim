78e69a74d08fe4e00e6623e0d7d5ae288e653118
Sergio-dot <sergio.siclari@outlook.com>
2026-05-19 17:03:21 +0200

    feat: add oil.nvim for editing file system like a buffer

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+}
