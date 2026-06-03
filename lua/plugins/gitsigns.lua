7f6ee34090cb7e9ba550045a01de1de3610a2714
Sergio-dot <sergio.siclari@outlook.com>
2026-06-03 09:27:14 +0200

    add abbreviated sha to git blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,13 +7,24 @@ return {
 			delete = { text = "_" },
 		},
 		current_line_blame = true,
+
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "right_align",
+			delay = 300,
+		},
+
+		current_line_blame_formatter = "<author> • <author_time:%R> • <abbrev_sha>",
+
 		signcolumn = true,
 		numhl = false,
 		linehl = false,
+
 		watch_gitdir = {
 			interval = 1000,
 			follow_files = true,
 		},
+
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
@@ -48,6 +59,7 @@ return {
 			map("n", "<leader>ghp", gs.preview_hunk, { desc = "Git: Preview hunk" })
 			map("n", "<leader>ghr", gs.reset_hunk, { desc = "Git: Reset hunk" })
 			map("n", "<leader>gd", gs.diffthis, { desc = "Git: Diff this file" })
+
 			map("n", "<leader>gD", function()
 				gs.diffthis("~")
 			end, { desc = "Git: Diff this file (against HEAD~1)" })
